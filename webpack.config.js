1385fe722c170eee5537b9182739a58c9fbbe0df
Shane Morgan <shane@cd2solutions.co.uk>
2018-02-17 17:22:27 +0000

    v3.0.11

--- webpack.config.js
+++ webpack.config.js
@@ -43,5 +43,7 @@ module.exports = {
   externals: {
     react: "commonjs react",
     "react-dom": "commonjs react-dom",
+    "mobx": "commonjs mobx",
+    "mobx-react": "commonjs mobx-react",
   },
 }
