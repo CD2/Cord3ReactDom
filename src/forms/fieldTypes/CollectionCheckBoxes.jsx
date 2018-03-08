c9de7f5d9abc633655425cfbf210d4a06967802c
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2018-03-08 15:54:39 +0000

    tag selector!

--- src/forms/fieldTypes/CollectionCheckBoxes.jsx
+++ src/forms/fieldTypes/CollectionCheckBoxes.jsx
@@ -13,13 +13,14 @@ export default class CollectionCheckBoxes extends React.Component {
   }
 
   async componentDidMount() {
+    this.values = this.props.values
     const { name_attribute, value_attribute, custom_attribute, collection } = this.props
     if (!collection) throw new Error(`You have not sent a collection - check your field!`)
     this.choices = await collection.pluck(value_attribute, name_attribute, custom_attribute)
   }
 
   @observable choices
-  @observable values = []
+  @observable values
 
   @action
   handleChange = value => {
