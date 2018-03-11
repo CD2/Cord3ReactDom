dd05b6095507361812520288f4f1f8cc9d4f7cb1
Shane Morgan <shane@cd2solutions.co.uk>
2018-03-11 16:53:08 +0000

    label

--- src/forms/fieldTypes/image.jsx
+++ src/forms/fieldTypes/image.jsx
@@ -41,17 +41,19 @@ export default class ImageField extends React.Component {
     if (!value.allowedTypes) throw new Error(`YOU MUST SET DEFAULT TYPES IN THE MODEL!!!!!!!!`)
     return (
       <React.Fragment>
-        <div className={`image-field${!value || !value.url ? ` unselected` : ``}`}>
-          <div>{this.renderPreview()}</div>
-        </div>
-        <input
-          type="file"
-          name={name}
-          multiple={multiple}
-          accept={value.allowedTypes.map(x => `.${x}`).join(`,`)}
-          onChange={this.handleChange}
-          onFocus={onFocus}
-        />
+        <label style={{ cursor: "pointer" }}>
+          <div className={`image-field${!value || !value.url ? ` unselected` : ``}`}>
+            <div>{this.renderPreview()}</div>
+          </div>
+          <input
+            type="file"
+            name={name}
+            multiple={multiple}
+            accept={value.allowedTypes.map(x => `.${x}`).join(`,`)}
+            onChange={this.handleChange}
+            onFocus={onFocus}
+          />
+        </label>
       </React.Fragment>
     )
   }
