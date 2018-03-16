6430f9d9d9f57df15ec6cdc2b952b16b8fbef2a4
Will <will.umeh.cd2@solutions.co.uk>
2018-03-16 15:19:56 +0000

    confliccliclc

--- src/ComponentWithRecord.jsx
+++ src/ComponentWithRecord.jsx
@@ -12,21 +12,21 @@ export default class ComponentWithRecord extends React.Component {
     this.render = this.mainRender
   }
 
-  @computed
-  get id() {
-    return this._id || this.props.id
-  }
-
-  set id(val) {
-    this._id = val
-  }
-
   componentDidMount() {
     invariant(this.Model !== undefined, `Model must be set`)
     invariant(this.id !== undefined, `ID must be set`)
     this.getRecord()
 
     reaction(() => this.id, () => this.getRecord(), true)
+  }
+
+  @computed
+  get id() {
+    return  this._id || this.props.id
+  }
+
+  set id(val) {
+    this._id = val
   }
 
   async getRecord() {
