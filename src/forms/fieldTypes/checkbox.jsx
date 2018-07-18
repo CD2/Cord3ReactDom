46c5063806b03ab6083bcfb2c6e9275e168b5f63
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2018-07-18 16:45:16 +0100

    uses a value supplied if supplied

--- src/forms/fieldTypes/checkbox.jsx
+++ src/forms/fieldTypes/checkbox.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { observer } from "mobx-react"
-import { observable } from "mobx"
+import {
+  observer
+} from "mobx-react"
+import {
+  observable
+} from "mobx"
 
 @observer
 export class Checkbox extends React.Component {
@@ -24,7 +28,10 @@ export class Checkbox extends React.Component {
   @observable checked = this.props.value
 
   handleChange = e => {
-    const { onRawChange, onChange } = this.props
+    const {
+      onRawChange,
+      onChange
+    } = this.props
     if (onRawChange) onRawChange(e)
     if (onChange) {
       onChange(e.target.checked)
@@ -32,41 +39,73 @@ export class Checkbox extends React.Component {
     }
   }
 
-  hasCheckedProp(checked){
-    if(this.props.checked === false ||this.props.checked ){
+  hasCheckedProp(checked) {
+    if (this.props.checked === false || this.props.checked) {
       this.checked = this.props.checked
     } else {
       this.checked = checked
     }
   }
 
-  checkedOrDefault(){
-    if(this.checked === false || this.checked) return this.checked
+  checkedOrDefault() {
+    if (this.props.value === false || this.props.value) return this.props.value
+    if (this.checked === false || this.checked) return this.checked
     return this.props.defaultValue
   }
 
   render() {
-    const { fieldTitle, customLabel } = this.props
-    return (
-      <label style={{ marginTop: `8px`, verticalAlign: `middle`, ...this.props.style }}>
-        <input
-          className="checkbox"
-          checked={this.checkedOrDefault()}
-          type="checkbox"
-          name={this.props.name}
-          style={{
-            display: `inline-block`,
-            verticalAlign: `middle`,
-          }}
-          onChange={this.handleChange}
-        />
-        <div style={{ display: `inline-block`, width: `20px`, verticalAlign: `middle` }}>
-          <span className="check-box" />
-        </div>
-        <span style={{ display: `inline-block`, verticalAlign: `middle` }}>
-          {customLabel || fieldTitle}
-        </span>
-      </label>
+    const {
+      fieldTitle,
+      customLabel
+    } = this.props
+    return ( <
+      label style = {
+        {
+          marginTop: `8px`,
+          verticalAlign: `middle`,
+          ...this.props.style
+        }
+      } >
+      <
+      input className = "checkbox"
+      checked = {
+        this.checkedOrDefault()
+      }
+      type = "checkbox"
+      name = {
+        this.props.name
+      }
+      style = {
+        {
+          display: `inline-block`,
+          verticalAlign: `middle`,
+        }
+      }
+      onChange = {
+        this.handleChange
+      }
+      /> <
+      div style = {
+        {
+          display: `inline-block`,
+          width: `20px`,
+          verticalAlign: `middle`
+        }
+      } >
+      <
+      span className = "check-box" / >
+      <
+      /div> <
+      span style = {
+        {
+          display: `inline-block`,
+          verticalAlign: `middle`
+        }
+      } > {
+        customLabel || fieldTitle
+      } <
+      /span> < /
+      label >
     )
   }
 }
